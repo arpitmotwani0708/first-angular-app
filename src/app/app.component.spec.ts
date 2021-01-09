9f077d2ef8d1760eedf3bd517f1371c5f0e6d0db
arpitmotwani0708 <arpit12221@gmail.com>
2021-01-10 00:42:36 +0530

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-angular-app'`, () => {
+  it(`should have as title 'first-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-angular-app');
+    expect(app.title).toEqual('first-angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-first-angular-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('first-angular-app app is running!');
   });
 });
