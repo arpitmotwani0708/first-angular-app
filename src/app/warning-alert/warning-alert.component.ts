2b00ff9158d1a45b6df09a669494bcf3348817be
arpitmotwani0708 <arpit12221@gmail.com>
2021-01-10 04:16:08 +0530

    changed naming of component selector for success and warning component

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-warningalert',
+    selector: 'app-warning-alert',
     templateUrl: './warning-alert.component.html',
     styleUrls: ['./warning-alert.component.css']
 })
