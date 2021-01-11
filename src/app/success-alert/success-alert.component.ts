33eab50ccad03012d0d929e2c5d8fd6f6d050aea
arpitmotwani0708 <arpit12221@gmail.com>
2021-01-11 22:55:49 +0530

    Removed warning and success alert components

--- src/app/success-alert/success-alert.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector:'app-successalert',
-    templateUrl: './success-alert.component.html',
-    styleUrls: ['./success-alert.component.css']
-})
-export class SuccessAlertComponent {
-}
--- src/app/warning-alert/warning-alert.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-warningalert',
-    templateUrl: './warning-alert.component.html',
-    styleUrls: ['./warning-alert.component.css']
-})
-export class WarningAlertComponent {
-}
