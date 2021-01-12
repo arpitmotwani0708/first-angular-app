08c43c0cc1e826d4365fd984187f6a491815d9b3
arpitmotwani0708 <arpit12221@gmail.com>
2021-01-13 01:30:44 +0530

    2 way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No Server was created!';
-  serverName = '';
+  serverName = 'TestServer';
 
   constructor() {
     setTimeout(()=>{
