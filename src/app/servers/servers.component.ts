be6beb82869157a03e57c84faabb22d84ec99d26
arpitmotwani0708 <arpit12221@gmail.com>
2021-01-14 00:07:43 +0530

    ngFor directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,33 +1,35 @@
-import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styles: [`
-    .online {
-      color: white
-    }
-  `]
+  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  serverId:number = 10;
-  serverStatus:string = 'offline'
-  
-  constructor() {
-    this.serverStatus = (Math.random() > 0.5) ? 'online' : 'offline';
+  serverName = 'TestServer'
+  allowedServer = false;
+  serverCreated = false;
+  serverCreationStatus = 'No server was created!';
+  servers = ['Test Server', 'Heroku Server'];
+
+  constructor() { 
+    setTimeout(()=>{
+      this.allowedServer = true;
+    },2000);
   }
 
   ngOnInit(): void {
   }
 
-  getServerStatus() {
-    return this.serverStatus;
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is '+this.serverName;
   }
 
-  getColor() {
-    return this.serverStatus === 'online' ? 'green' : 'red';
+  onUpdateServerName(event:Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
